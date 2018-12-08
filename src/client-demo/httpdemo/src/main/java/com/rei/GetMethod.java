f2e579f8ed1f532be2135c7837e182004d50a837
jyxk <jyxking007@gmail.com>
2018-12-08 16:53:13 +0800

    finished the http demo

---
+++ src/client-demo/httpdemo/src/main/java/com/rei/GetMethod.java
@@ -0,0 +1,21 @@
+package com.rei;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class GetMethod {
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url, String cmd) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .header("CMD", cmd)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+}
--- src/client/src/lmss/view/Controller.java
+++ src/client/src/lmss/view/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package lmss.view;
 
 public class Controller {
 }
