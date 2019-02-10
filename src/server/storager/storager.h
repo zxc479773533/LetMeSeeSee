bed6f82a4968a8005cf28bf8e3e5ba6c0ea6f699
Lab-Zjt <a1322144594@gmail.com>
2019-02-10 16:07:52 +0800

    update logger

--- src/server/storager/storager.h
+++ src/server/storager/storager.h
@@ -7,7 +7,8 @@
 namespace lmss {
   // 单例模式
   // With C++11, static variable's construction is thread-safety.
-  template <typename T> class Singleton {
+  template<typename T>
+  class Singleton {
   public:
     static T &GetInstance() {
       static T ins{};
@@ -36,7 +37,9 @@ namespace lmss {
     // 扫描源码，并将所有数据储存节点转换为json格式保存。
     void ScanSourceCode(const std::string &path);
     // 返回节点列表。
-    std::vector<std::string> &GetNodeList() { return _node_list; }
+    std::vector<std::string> &GetNodeList() {return _node_list;}
+    // 设置日志文件，默认为std::clog，如果为空则不打印日志。
+    void SetLogFile(const std::string &filename);
   };
 // Store宏，第一个参数是数据储存节点的名称，之后的参数为该节点储存的数据的名称。
 // 通过helper变量调用Storager的注册函数。
