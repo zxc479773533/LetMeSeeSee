dff280b2bdab42d1746961b677dc53ba7b28cca3
jyxk <jyxking007@gmail.com>
2019-02-24 21:41:19 +0800

    i am stupid, now it has been fixed

---
+++ src/server/storager/storager.cpp
@@ -0,0 +1,90 @@
+#include <thread>
+#include "storager.h"
+#include "../utility/file.h"
+#include "../utility/io.h"
+#include "../net/listener.h"
+#include "../net/address.h"
+#include "../net/connection.h"
+#include "../net/request.h"
+
+namespace lmss {
+  std::string Storager::Call(const std::string &name) {
+    auto &f = reflector::GetInstance()[name];
+    if (f) {
+      return f();
+    }
+    srlib::logln("Reflector Warning: No Matching Function:", name);
+    return {};
+  }
+  void Storager::ScanSourceCode(const std::string &path) {
+    // 遍历目录，对目录下的每一个文件进行扫描。
+    srlib::OpenDir(path).RecursiveWalk([this](srlib::FileInfo &&info) {
+      std::string reg(R"(^ *Store\(([^)]*)\).*$)");
+      for (auto &line:srlib::OpenFile(info.Path()).ReadAll().split("\n")) {
+        // 找到包含了Store宏的行。
+        if (line.regex_match(reg)) {
+          auto vec = line.regex_replace(reg, "$1").split(",");
+          for (auto &v:vec) {
+            v = v.trim();
+          }
+          // json化
+          auto json = srlib::String(R"({"node": ")") + vec[0] + "\", " + R"("data": [)";
+          for (int i = 1; i < vec.size(); ++i) {
+            if (i == vec.size() - 1) {
+              json += '"' + vec[i] + "\"";
+            } else {
+              json += '"' + vec[i] + "\", ";
+            }
+          }
+          json += "]}";
+          _node_list.emplace_back(std::move(json.std_string_copy()));
+        }
+      }
+    });
+  }
+  void Storager::ListenAndServe(const std::string &ip, uint16_t port) {
+    using namespace srlib;
+    std::thread([&ip, port, this]() {
+      net::Listener listener(net::Address(ip, port));
+      while (true) {
+        auto conn = listener.Accept();
+        std::thread([this](std::shared_ptr<net::Connection> conn) {
+          while (true) {
+            auto req = net::RecvHTTPRequest(*conn);
+            // println(req.Serialize());
+            if (req.version.empty())return;
+            // Request NodeList
+            if (req.page == "/nodelist") {
+              srlib::String json;
+              for (auto &j : _node_list) {
+                json += j + "\n";
+              }
+              net::SendHTTPResponse(*conn,
+                                    net::HTTPResponse{}.AutoFill()
+                                                       .Header("Content-Length", std::to_string(json.size()))
+                                                       .Content(json));
+            } else {
+              // Request Node
+              auto node_name = req.page(req.page.find('/') + 1, req.page.size());
+              auto file_name = Storager::Call(node_name.std_string());
+              if (!file_name.empty()) {
+                auto file = OpenFile(file_name);
+                net::SendHTTPResponse(*conn,
+                                      net::HTTPResponse{}.AutoFill()
+                                                         .Header("Content-Length", std::to_string(file.Size()))
+                                                         .Content(file.ReadAll()));
+              } else {
+                net::SendHTTPResponse(*conn,
+                                      net::HTTPResponse{}.Version("1.1")
+                                                         .StatusCode("404")
+                                                         .Header("Content-Length", std::to_string(13))
+                                                         .ReasonPhrase("Not Found")
+                                                         .Content("404 not found"));
+              }
+            }
+          }
+        }, conn).detach();
+      }
+    }).detach();
+  }
+}
---
+++ src/server/storager/storager.h
@@ -0,0 +1,45 @@
+#ifndef LETMESEESEE_STORE_H
+#define LETMESEESEE_STORE_H
+
+#include <unordered_map>
+#include <vector>
+#include "../utility/singleton.h"
+
+namespace lmss {
+  
+  // 数据储存节点函数类型。
+  typedef std::string(*store_t)();
+  // 假反射模板类。
+  template<typename T> using Reflector = std::unordered_map<std::string, T>;
+  
+  class Storager {
+  private:
+    // 使用单例模式避免注册函数在Reflector初始化前被调用。
+    typedef srlib::Singleton<Reflector<store_t>> reflector;
+  private:
+    // json格式的节点列表。
+    std::vector<std::string> _node_list;
+  public:
+    // 注册函数，将一个函数注册到Reflector中。一般来说只会通过Store宏被调用。
+    static void Register(const std::string &name, store_t func) {reflector::GetInstance()[name] = func;}
+    // 通过函数名调用函数，返回数据保存的文件的位置。
+    static std::string Call(const std::string &name);
+    void ListenAndServe(const std::string &ip, uint16_t port);
+    // 扫描源码，并将所有数据储存节点转换为json格式保存。
+    void ScanSourceCode(const std::string &path);
+    // 返回节点列表。
+    std::vector<std::string> &GetNodeList() {return _node_list;}
+  };
+// Store宏，第一个参数是数据储存节点的名称，之后的参数为该节点储存的数据的名称。
+// 通过helper变量调用Storager的注册函数。
+// Usage:
+// Store(Node1,Data1,Data2){
+//   do_something();
+//   return file_name;
+// }
+#define Store(name, ...) std::string name();\
+static auto name##helper = (lmss::Storager::Register(#name,name),0);\
+std::string name()
+}
+
+#endif
